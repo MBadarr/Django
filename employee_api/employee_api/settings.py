2f48ab5dbe6e742fccefdfde98aa9622acacdcfd
badar-vulcantech <muhammad.badar@vulcantech.co>
2025-01-29 11:23:35 +0500

    DRF Setup

--- employee_api/employee_api/settings.py
+++ employee_api/employee_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'employees',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,12 @@ WSGI_APPLICATION = 'employee_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'employee_db',
+        'USER': 'postgres',
+        'PASSWORD': 'vulcantech',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
